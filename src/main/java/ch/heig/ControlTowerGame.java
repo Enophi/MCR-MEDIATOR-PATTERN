bcad9c54112d8049bea9d2101c0e2c0ffc303606
David Cruchon <david@enophi.ch>
2019-05-12 12:54:34 +0200

    Gradle config

---
+++ src/main/java/ch/heig/ControlTowerGame.java
@@ -0,0 +1,11 @@
+package ch.heig;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.settings.GameSettings;
+
+public class ControlTowerGame extends GameApplication {
+    @Override
+    protected void initSettings(GameSettings settings) {
+
+    }
+}
